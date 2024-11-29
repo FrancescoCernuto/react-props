70a3730be482349340f7ad53e55c7c80491956ca
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-29 17:41:00 +0100

    import post + add tagLis

--- vite-project/src/assets/components/Main.jsx
+++ vite-project/src/assets/components/Main.jsx
@@ -1,2 +1,12 @@
 import Filter from "./Filter";
 import Card from "./Card";
+
+import { posts } from "../data/posts";
+
+const tagsList = [];
+posts.map((post, i) => {
+    post.tags.map((tag) => {
+        if (!tagsList.includes(tag)) tagsList.push(tag);
+    });
+});
+console.log(tagsList);
