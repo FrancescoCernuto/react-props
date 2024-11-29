513e3d866725007cdd32d93fdd3164787967e896
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-29 15:04:41 +0100

    first commit

---
+++ vite-project/src/assets/components/Card.jsx
@@ -0,0 +1,24 @@
+import Button from "./Button";
+export default function Card() {
+    return (
+        <>
+            <div className="card">
+                <div className="card-image">
+                    <img
+                        src="https://placehold.co/600x400/EEE/31343C"
+                        alt="place-holder"
+                    />
+                </div>
+                <div className="card-content">
+                    <h4>Titolo del Post</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat
+                        explicabo eius pariatur perferendis commodi, voluptatum harum
+                        exercitationem!.
+                    </p>
+                    <Button />
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ vite-project/src/assets/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (<>
+        <header>
+            <h1 className="text-center">Il mio Blog</h1>
+        </header>
+    </>
+    );
+}
